08e4eac9fe3af999bf80bdedf830cf624b3bd077
David Kerekes <davkedav@gmail.com>
2020-07-16 20:30:07 +0200

    'use strict' everywhere

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3030;
-const router = require('./router');
+const router = require('./routers');
 
-app.use(router);
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
